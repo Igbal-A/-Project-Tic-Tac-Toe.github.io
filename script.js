f00b33bf8320a0f2d97f98a0e5c7b751e4dd418e
Igbal <igbal.alyasov@mail.ru>
2025-01-09 19:44:32 +0300

    mvc

--- script.js
+++ script.js
@@ -1,6 +1,115 @@
+function GameModal () {
+    let board = [];
+    let gameActive = true;
+    let currentPlayer = 'x';
+
+    const winConditions = [
+        [0, 1, 2], // Top row
+        [3, 4, 5], // Middle row
+        [6, 7, 8], // Bottom row
+        [0, 3, 6], // Left column
+        [1, 4, 7], // Middle column
+        [2, 5, 8], // Right column
+        [0, 4, 8], // Left-to-right diagonal
+        [2, 4, 6],  // Right-to-left diagonal
+    ];
+
+    return {
+        board,
+        gameActive,
+        currentPlayer,
+        winConditions
+    }
+}
+
+function GameModelController () {
+    let gameModal = GameModal();
+
+    function winnerOrDraw (bthArgument, result) {
+        let btnIndex = bthArgument.target.id - 1;
+        if (gameModal.board[btnIndex] !== undefined || !gameModal.gameActive) {
+            return;
+        }
+        bthArgument.target.innerText = gameModal.currentPlayer;
+        gameModal.board[btnIndex] = gameModal.currentPlayer;
+        gameModal.currentPlayer = gameModal.currentPlayer === 'x' ? 'o' : 'x';
+
+        for (let i = 0; i < gameModal.winConditions.length; i++) {
+            const [a, b, c] = gameModal.winConditions[i];
+            if (gameModal.board[a] && gameModal.board[a] === gameModal.board[b] && gameModal.board[a] === gameModal.board[c]) {
+                if (gameModal.board[a] === 'x') {
+                    result.innerText = 'result: win X'
+                    gameModal.gameActive = false;
+                    return 
+                }
+                if (gameModal.board[a] === 'o') {
+                    result.innerText = 'result: win O'
+                    gameModal.gameActive = false;
+                    return 
+                }
+            }
+        }
+        let roundDraw = !gameModal.board.includes(undefined);
+        if (roundDraw && gameModal.board.length == 9) {
+            result.innerText = 'result: draw'
+            gameModal.gameActive = false;
+            return 
+        }
+    }
+
+    function restartGame (buttons, result) {
+        buttons.forEach(btn => {
+            btn.innerText = '';
+        });
+        result.innerText = 'result: '
+        gameModal.gameActive = true;
+        gameModal.board = [];
+    }
+
+    return {
+        restartGame,
+        winnerOrDraw
+    }
+}
+
+function GameModelView () {
+    let gameModelController = GameModelController();
+
+    let buttons = document.querySelectorAll('.button-board');
+    let btnRestart = document.querySelector('.restart');
+    let result = document.querySelector('.result');
+
+    function btnClick () {
+        buttons.forEach(btn => {
+            btn.addEventListener('click', (button) => {
+                gameModelController.winnerOrDraw(button, result)
+            })
+        })
+    
+        btnRestart.addEventListener('click', () => {
+            gameModelController.restartGame(buttons, result);
+        })
+    }
+    return {btnClick}
+}
 
 
-function Gameboard () {
+(function runApp () {
+    try {
+        const game = GameModelView();
+        game.btnClick();
+    } catch (error) {
+        console.error(error);
+    }
+})();
+
+
+
+
+
+
+
+/* function Gameboard () {
     let board = [];
     let gameActive = true;
     let currentPlayer = 'x';
@@ -82,8 +191,5 @@ function eventClicks () {
     })
 }
 
-eventClicks();
-
-
-
+eventClicks(); */
 
